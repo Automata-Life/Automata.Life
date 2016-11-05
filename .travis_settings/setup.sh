d89547fc07fe8463dd507ce7d0894500180373f4
Pedro Bruel <pedro.bruel@gmail.com>
2016-11-05 15:25:05 -0200

    Fix paths inside travis setup script.

--- .travis_settings/setup.sh
+++ .travis_settings/setup.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-TRAVIS_DB=$(cat travis_database.txt)
+TRAVIS_DB=$(cat .travis_settings/travis_database.txt)
 
-SETTINGS_FILE=../automata_life_server/settings.py
+SETTINGS_FILE=automata_life_server/settings.py
 
 sed -zi "s/DATABASES[^}]*./$TRAVIS_DB/g" "$SETTINGS_FILE"
