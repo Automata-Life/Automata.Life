f31cd273c025806323deaf3b04ed01f22d533081
Pedro Bruel <pedro.bruel@gmail.com>
2016-11-05 21:35:10 -0200

    Add 'hexagon.js' to sample page.

--- automata_life/views.py
+++ automata_life/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
+from django.template import Context, loader
 
 def index(request):
     return HttpResponse("Hello, broa. Follow your heart.")
-# Create your views here.
+
+def game(request):
+    template = loader.get_template("game.html")
+    return HttpResponse(template.render())
