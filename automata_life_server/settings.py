9d53043efb815f886c53db29c63514c60b21e954
phrb <pedro.bruel@gmail.com>
2016-11-09 17:02:00 -0200

    Remove material UI.
    @phrb, @carybe, @rilifon, @caio1

--- automata_life_server/settings.py
+++ automata_life_server/settings.py
@@ -31,9 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-	'material',
-	'material.frontend',
-	'material.admin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
